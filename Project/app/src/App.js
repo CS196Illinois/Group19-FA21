89aaf0cfee235235fa7402ac85df32c36864f3b4
diegotaveras <diegoat2@illinois.edu>
2021-11-07 23:32:03 -0600

    fixed buttons and added next redirect file

--- Project/app/src/App.js
+++ Project/app/src/App.js
@@ -1,17 +1,28 @@
 import React from "react";
+// import { Route, Routes } from 'react-router-dom';
+
+// const history = useHistory();
+
+//   const routeChange = () =>{ 
+//     let path = `\App2.js`; 
+//     gonk.history.push(path);
+//   }
+
 
 function clickMe() {
-    alert('You clicked me');
+  alert('You clicked me');
 }
 export default function App() {
     return (
-        <div>
-            <button onClick = {clickMe}>
-              Fancy
-            </button>
-            <button onClick = {clickMe}>
-              Cheap
-            </button>
+        <div style={{ position: 'absolute', left: '50%', top: '50%',
+        transform: 'translate(-50%, -50%)'}}>
+          <button onClick = {clickMe}>
+            Fancy
+          </button>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <button onClick = {clickMe}>
+            Cheap
+          </button>
         </div>
     )
 }
